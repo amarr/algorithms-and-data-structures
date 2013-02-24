ac151afcb0101e05aa8a7aaec4cc57b85bc3e748
System Administrator <root@SDGL11fdb002d.local>
2013-02-24 15:18:32 -0800

    Updating

--- linked-list.js
+++ linked-list.js
@@ -6,23 +6,25 @@
 		this._next = null;
 	}
 
-	Element.prototype.next = function(element) {
-		if(element === undefined) {
-			return this._next;
-		}
-		else {
-			this._next = element;
-		}
-	}
+	Element.prototype = {
+		next : function(element) {
+			if(element === undefined) {
+				return this._next;
+			}
+			else {
+				this._next = element;
+			}
+		},
 
-	Element.prototype.body = function(body) {
-		if(body === undefined) {
-			return this._body;
+		body : function(body) {
+			if(body === undefined) {
+				return this._body;
+			}
+			else {
+				this._body = body;
+			}
 		}
-		else {
-			this._body = body;
-		}
-	}
+	};
 
 	// List class
 	function LinkedList() {
@@ -30,49 +32,51 @@
 		this._tail  = null;
 	}
 
-	LinkedList.prototype.add = function(value) {
-		if(this._head == null) {
-			var el = new Element(value);
-			this._head = el;
-			this._tail = el;
-		}
-		else {
-			this._tail = this.addAfter(this._tail, value);
-		}
-	}
-
-	LinkedList.prototype.addAfter = function(el, value) {
-		var newEl = new Element(value);
-
-		newEl.next(el.next());
-		el.next(newEl);
-
-		return newEl;
-	}
-
-	LinkedList.prototype.removeAfter = function(el) {
-		var removedEl = el.next();
-
-		if(removedEl != null) {
-			if(removedEl.next() != null) {
-				el.next(removedEl.next());
+	LinkedList.prototype = {
+		add : function(value) {
+			if(this._head == null) {
+				var el = new Element(value);
+				this._head = el;
+				this._tail = el;
 			}
 			else {
-				el.next(null);
+				this._tail = this.addAfter(this._tail, value);
 			}
+		},
+
+		addAfter : function(el, value) {
+			var newEl = new Element(value);
+
+			newEl.next(el.next());
+			el.next(newEl);
+
+			return newEl;
+		},
+
+		removeAfter : function(el) {
+			var removedEl = el.next();
+
+			if(removedEl != null) {
+				if(removedEl.next() != null) {
+					el.next(removedEl.next());
+				}
+				else {
+					el.next(null);
+				}
+			}
+
+			return removedEl;
+		},
+
+		head : function() {
+			return this._head;
+		},
+
+		empty : function() {
+			this._head = null;
+			this._tail = null;
 		}
-
-		return removedEl;
-	}
-
-	LinkedList.prototype.head = function() {
-		return this._head;
-	}
-
-	LinkedList.prototype.empty = function() {
-		this._head = null;
-		this._tail = null;
-	}
+	};
 
 	root.LinkedList = LinkedList;
 })(this);
