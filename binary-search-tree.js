451884e457677b66a06e9c80f0c9b3a1a6a79081
amarr <marron.austin@gmail.com>
2013-02-25 21:18:23 -0800

    Formatting update

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,255 +1,255 @@
 /* Binary Search Tree */
 (function(root) {
-	var LESS = -1;
-	var GREATER = 1;
-	var EQUAL = 0;
+    var LESS = -1;
+    var GREATER = 1;
+    var EQUAL = 0;
 
-	// Element class
-	function Element(key, record) {
-		this._key = key;
-		this.record = record;
-		this._left = null;
-		this._right = null;
-		this._parent = null;
-	}
+    // Element class
+    function Element(key, record) {
+        this._key = key;
+        this.record = record;
+        this._left = null;
+        this._right = null;
+        this._parent = null;
+    }
 
-	Element.prototype = {
-		compareTo : function(key) {
-			if(key instanceof Element) {
-				key = key.key();
-			}
-			
-			if(this.key() < key) {
-				return LESS;
-			}
-			else if(this.key() > key) {
-				return GREATER;
-			}
-			else {
-				return EQUAL;
-			}
-		},
+    Element.prototype = {
+        compareTo : function(key) {
+            if(key instanceof Element) {
+                key = key.key();
+            }
+            
+            if(this.key() < key) {
+                return LESS;
+            }
+            else if(this.key() > key) {
+                return GREATER;
+            }
+            else {
+                return EQUAL;
+            }
+        },
 
-		is : function(el) {
-			return this.key() == el.key();
-		},
+        is : function(el) {
+            return this.key() == el.key();
+        },
 
-		key : function() {
-			return this._key;
-		},
+        key : function() {
+            return this._key;
+        },
 
-		left : function(el) {
-			if(arguments.length == 1) {				
-				this._left = el;
-				
-				if(el != null) {
-					el.parent(this);
-				}
-			}
+        left : function(el) {
+            if(arguments.length == 1) {             
+                this._left = el;
+                
+                if(el != null) {
+                    el.parent(this);
+                }
+            }
 
-			return this._left;
-		},
+            return this._left;
+        },
 
-		right : function(el) {
-			if(arguments.length == 1) {				
-				this._right = el;
+        right : function(el) {
+            if(arguments.length == 1) {             
+                this._right = el;
 
-				if(el != null) {
-					el.parent(this);
-				}
-			}
+                if(el != null) {
+                    el.parent(this);
+                }
+            }
 
-			return this._right;
-		},
+            return this._right;
+        },
 
-		parent : function(el) {
-			if(arguments.length == 1) {				
-				this._parent = el;
-			}
+        parent : function(el) {
+            if(arguments.length == 1) {             
+                this._parent = el;
+            }
 
-			return this._parent;
-		},
+            return this._parent;
+        },
 
-		hasNoChildren : function() {
-			return !this.hasLeftChild() && !this.hasRightChild();
-		},
+        hasNoChildren : function() {
+            return !this.hasLeftChild() && !this.hasRightChild();
+        },
 
-		hasNoParent : function() {
-			return this.parent() == null;
-		},
+        hasNoParent : function() {
+            return this.parent() == null;
+        },
 
-		hasLeftChild : function() {
-			return this.left() != null;
-		},
+        hasLeftChild : function() {
+            return this.left() != null;
+        },
 
-		hasRightChild : function() {
-			return this.right() != null;
-		},
+        hasRightChild : function() {
+            return this.right() != null;
+        },
 
-		hasOneChild : function() {
-			return (this.hasRightChild() && !this.hasLeftChild()) || (!this.hasRightChild() && this.hasLeftChild());
-		},
+        hasOneChild : function() {
+            return (this.hasRightChild() && !this.hasLeftChild()) || (!this.hasRightChild() && this.hasLeftChild());
+        },
 
-		onlyChild : function() {
-			if(this.hasOneChild()) {
-				return this.right() != null ? this.right() : this.left();
-			}
-		},
+        onlyChild : function() {
+            if(this.hasOneChild()) {
+                return this.right() != null ? this.right() : this.left();
+            }
+        },
 
-		replaceWith : function(el) {
-			if(this.hasNoParent()) {
-				throw 'No parent!';
-				return;
-			}
-			else if(this.parent().left().is(this)) {
-				this.parent().left(el);
-			}
-			else if(this.parent().right().is(this)) {
-				this.parent().right(el);
-			}
+        replaceWith : function(el) {
+            if(this.hasNoParent()) {
+                throw 'No parent!';
+                return;
+            }
+            else if(this.parent().left().is(this)) {
+                this.parent().left(el);
+            }
+            else if(this.parent().right().is(this)) {
+                this.parent().right(el);
+            }
 
-			if(el == null) {
-				return;
-			}
+            if(el == null) {
+                return;
+            }
 
-			el.parent(this.parent());
+            el.parent(this.parent());
 
-			if(this.hasLeftChild() && !this.left().is(el)) {
-				el.left(this.left());
-				this.left().parent(el);
-			}
+            if(this.hasLeftChild() && !this.left().is(el)) {
+                el.left(this.left());
+                this.left().parent(el);
+            }
 
-			if(this.hasRightChild() && !this.right().is(el)) {
-				el.right(this.right());
-				this.right().parent(el);
-			}
-		}
-	};
+            if(this.hasRightChild() && !this.right().is(el)) {
+                el.right(this.right());
+                this.right().parent(el);
+            }
+        }
+    };
 
-	function BinarySearchTree() {
-		this._root = null;
-		this._replaceMethod = 0;
-	};
+    function BinarySearchTree() {
+        this._root = null;
+        this._replaceMethod = 0;
+    };
 
-	/**
-	 * BinarySearchTree
-	 * @type {Class}
-	 */
-	BinarySearchTree.prototype = {
-		search : function(key, root) {
-			if(arguments.length == 1) {
-				return this.search(key, this._root);
-			}
+    /**
+     * BinarySearchTree
+     * @type {Class}
+     */
+    BinarySearchTree.prototype = {
+        search : function(key, root) {
+            if(arguments.length == 1) {
+                return this.search(key, this._root);
+            }
 
-			if(root == null) {
-				return null;
-			}
+            if(root == null) {
+                return null;
+            }
 
-			switch(root.compareTo(key)) {
-				case LESS:
-					return this.search(key, root.right());
-					break;
-				case GREATER:
-					return this.search(key, root.left());
-					break;
-				case EQUAL:
-					return root;
-					break;
-			}
-		},
+            switch(root.compareTo(key)) {
+                case LESS:
+                    return this.search(key, root.right());
+                    break;
+                case GREATER:
+                    return this.search(key, root.left());
+                    break;
+                case EQUAL:
+                    return root;
+                    break;
+            }
+        },
 
-		insert : function(key) {
-			var el = new Element(key);
-			if(this._root == null) {
-				this._root = el;
-				return el;
-			}
+        insert : function(key) {
+            var el = new Element(key);
+            if(this._root == null) {
+                this._root = el;
+                return el;
+            }
 
-			return this._insert(this._root, el);
-		},
+            return this._insert(this._root, el);
+        },
 
-		_insert : function(root, el) {
-			var parent = null;
+        _insert : function(root, el) {
+            var parent = null;
 
-			switch(root.compareTo(el)) {
-				case LESS:
-					parent = root.right();
-					if(parent == null) {
-						return root.right(el);
-					}
-					break;
-				case GREATER:
-					parent = root.left();
-					if(parent == null) {
-						return root.left(el);
-					}
-					break;
-				case EQUAL:
-					// Skip duplicate key
-					return;
-					break;
-			}
+            switch(root.compareTo(el)) {
+                case LESS:
+                    parent = root.right();
+                    if(parent == null) {
+                        return root.right(el);
+                    }
+                    break;
+                case GREATER:
+                    parent = root.left();
+                    if(parent == null) {
+                        return root.left(el);
+                    }
+                    break;
+                case EQUAL:
+                    // Skip duplicate key
+                    return;
+                    break;
+            }
 
-			return this._insert(parent, el);
-		},
+            return this._insert(parent, el);
+        },
 
-		remove : function(key) {
-			var el = null;
+        remove : function(key) {
+            var el = null;
 
-			if(key instanceof Element) {
-				el = key;
-			}
-			else {
-				el = this.search(key);
-			}
+            if(key instanceof Element) {
+                el = key;
+            }
+            else {
+                el = this.search(key);
+            }
 
-			if(el == null) {
-				return;
-			}
+            if(el == null) {
+                return;
+            }
 
-			return this._remove(el);
-		},
+            return this._remove(el);
+        },
 
-		_remove : function(el) {
-			// 0
-			if(el.hasNoChildren()) {
-				if(el.hasNoParent()) {
-					this._root = null;
-					return;
-				}
-				
-				el.replaceWith(null)
+        _remove : function(el) {
+            // 0
+            if(el.hasNoChildren()) {
+                if(el.hasNoParent()) {
+                    this._root = null;
+                    return;
+                }
+                
+                el.replaceWith(null)
 
-				return;
-			}
+                return;
+            }
 
-			// 1
-			if(el.hasOneChild()) {
-				if(el.hasNoParent()) {
-					this._root = null;
-					return;
-				}
+            // 1
+            if(el.hasOneChild()) {
+                if(el.hasNoParent()) {
+                    this._root = null;
+                    return;
+                }
 
-				el.replaceWith(el.onlyChild());
+                el.replaceWith(el.onlyChild());
 
-				return;
-			}
+                return;
+            }
 
-			// 2 - naive
-			if(this._replaceMethod == 0) {
-				this._replaceMethod = 1;
-				el.replaceWith(el.left());
-			}
-			else {
-				this._replaceMethod = 0;
-				el.replaceWith(el.right());
-			}
-		},
+            // 2 - naive
+            if(this._replaceMethod == 0) {
+                this._replaceMethod = 1;
+                el.replaceWith(el.left());
+            }
+            else {
+                this._replaceMethod = 0;
+                el.replaceWith(el.right());
+            }
+        },
 
-		root : function() {
-			return this._root;
-		}
-	};
+        root : function() {
+            return this._root;
+        }
+    };
 
-	root.BinarySearchTree = BinarySearchTree;
+    root.BinarySearchTree = BinarySearchTree;
 })(this);
--- linked-list.js
+++ linked-list.js
@@ -1,82 +1,82 @@
 /* Linked List **/
 (function(root) {
-	// Element class
-	function Element(body) {
-		this._body = body;
-		this._next = null;
-	}
+    // Element class
+    function Element(body) {
+        this._body = body;
+        this._next = null;
+    }
 
-	Element.prototype = {
-		next : function(element) {
-			if(element === undefined) {
-				return this._next;
-			}
-			else {
-				this._next = element;
-			}
-		},
+    Element.prototype = {
+        next : function(element) {
+            if(element === undefined) {
+                return this._next;
+            }
+            else {
+                this._next = element;
+            }
+        },
 
-		body : function(body) {
-			if(body === undefined) {
-				return this._body;
-			}
-			else {
-				this._body = body;
-			}
-		}
-	};
+        body : function(body) {
+            if(body === undefined) {
+                return this._body;
+            }
+            else {
+                this._body = body;
+            }
+        }
+    };
 
-	// List class
-	function LinkedList() {
-		this._head = null;
-		this._tail  = null;
-	}
+    // List class
+    function LinkedList() {
+        this._head = null;
+        this._tail  = null;
+    }
 
-	LinkedList.prototype = {
-		add : function(value) {
-			if(this._head == null) {
-				var el = new Element(value);
-				this._head = el;
-				this._tail = el;
-			}
-			else {
-				this._tail = this.addAfter(this._tail, value);
-			}
-		},
+    LinkedList.prototype = {
+        add : function(value) {
+            if(this._head == null) {
+                var el = new Element(value);
+                this._head = el;
+                this._tail = el;
+            }
+            else {
+                this._tail = this.addAfter(this._tail, value);
+            }
+        },
 
-		addAfter : function(el, value) {
-			var newEl = new Element(value);
+        addAfter : function(el, value) {
+            var newEl = new Element(value);
 
-			newEl.next(el.next());
-			el.next(newEl);
+            newEl.next(el.next());
+            el.next(newEl);
 
-			return newEl;
-		},
+            return newEl;
+        },
 
-		removeAfter : function(el) {
-			var removedEl = el.next();
+        removeAfter : function(el) {
+            var removedEl = el.next();
 
-			if(removedEl != null) {
-				if(removedEl.next() != null) {
-					el.next(removedEl.next());
-				}
-				else {
-					el.next(null);
-				}
-			}
+            if(removedEl != null) {
+                if(removedEl.next() != null) {
+                    el.next(removedEl.next());
+                }
+                else {
+                    el.next(null);
+                }
+            }
 
-			return removedEl;
-		},
+            return removedEl;
+        },
 
-		head : function() {
-			return this._head;
-		},
+        head : function() {
+            return this._head;
+        },
 
-		empty : function() {
-			this._head = null;
-			this._tail = null;
-		}
-	};
+        empty : function() {
+            this._head = null;
+            this._tail = null;
+        }
+    };
 
-	root.LinkedList = LinkedList;
+    root.LinkedList = LinkedList;
 })(this);
